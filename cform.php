dec2937771493cef979ba9bdbae1988eeab5ba89
Juan Morocho <jc23ace@gmail.com>
2018-07-23 17:10:14 -0500

    Website 1.0.0

--- cform.php
+++ cform.php
@@ -6,13 +6,13 @@
     $message = $_Post['message'];
     
 
-    $email_from = "$cEmail";
+    $email_from = 'jc23ace@gmail.com';
     $body = "$message";
 
-    $to= 'jc23ace@gmail.com';
+    $to = 'jc23ace@gmail.com';
 
     $headers ="From: $email_from \r\n";
-    $headers . = "Reply-To: $cEmail \r\n";
+    $headers .= "Reply-To: $cEmail \r\n";
 
     mail($to,$subject,$body,$headers);
 
