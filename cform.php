0940f4f6039686817af35926214b8b0c66dcba02
Juan Morocho <jc23ace@gmail.com>
2018-07-23 17:04:33 -0500

    Website 1.0.0

---
+++ cform.php
@@ -0,0 +1,21 @@
+<?php
+
+    $name = $_Post['name'];
+    $cEmail = $_Post['email'];
+    $subject = $_Post['subject'];
+    $message = $_Post['message'];
+    
+
+    $email_from = "$cEmail";
+    $body = "$message";
+
+    $to= 'jc23ace@gmail.com';
+
+    $headers ="From: $email_from \r\n";
+    $headers . = "Reply-To: $cEmail \r\n";
+
+    mail($to,$subject,$body,$headers);
+
+    header("Location: index.html");
+
+?>
